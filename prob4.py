ea00cc91d9cdfa5d4943f84fe59917c30c5a3361
Morgenrode <dsg14@zips.uakron.edu>
2014-07-28 08:34:30 -0500

    Finished prob 4

--- prob4.py
+++ prob4.py
@@ -1,8 +1,20 @@
 '''prob4.py: find the largest palidrome made form the product of two 3-digit numbers'''
 
-def isPalindrom(num):
+def isPalindrome(num):
          '''Test a string version of a number for palindromicity'''
-         return num[::-1] == num
+         return str(num)[::-1] == str(num)
 
-def search(x, y):
-        
+def search():
+         '''Search through all combinations of products of 3 digit numbers'''
+         palindrome = 0
+         for x in range(100,1000):
+                  for y in range(100,1000):
+                           z = x * y
+                           if isPalindrome(z) and z > palindrome:
+                                    palindrome = z
+                           else:
+                                    pass
+          
+         return palindrome
+
+print(search())
