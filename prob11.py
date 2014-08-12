5b3e63a3e91189e0e80b2ccd06307bbb4dfbeedc
Morgenrode <dsg14@zips.uakron.edu>
2014-08-12 00:14:27 -0500

    almost done with prob11, falling asleep, will finish tomorrow

--- prob11.py
+++ prob11.py
@@ -2,7 +2,7 @@
 20x20 grid of numbers'''
 
 def prod(iterable):
-'''Simple function to find the product of the input elements'''
+    '''Simple function to find the product of the input elements'''
     p = 1
     for n in iterable:
         p *= int(n)
@@ -12,8 +12,8 @@ grid = []
 product = 1
 
 with open('input.txt', 'r') as f:
-'''This makes every line in the input file a list of integers, and
-appends that list into a bigger list, grid'''
+    '''This makes every line in the input file a list of integers, and
+    appends that list into a bigger list, grid'''
     for line in f.readlines():
         line = line.replace(' ', ',').strip('\n')
         numline = []
@@ -21,7 +21,22 @@ appends that list into a bigger list, grid'''
             numline.append(int(block))
         grid.append(numline) 
 
-#try:
-#    product = prod(listx[i]...etc
-#except:
-#    pass
+j = 0
+while j < 20:
+    i = 0
+    while i < 20:
+        # Straight line path, left-to-right
+        try:
+            if prod(grid[j][i:i+4]) > product:
+                product = prod(grid[j][i:i+4])
+        except:
+            pass
+        # Straigh path, top-to-bottom
+        try:
+            if prod([grid[j][i], grid[j+1][i], \
+            grid[j+2][i], grid[j+3][i]]) > product:
+                product = prod([grid[j][i], grid[j+1][i], \
+            grid[j+2][i], grid[j+3][i]])
+        except:
+            pass
+        print(product)
