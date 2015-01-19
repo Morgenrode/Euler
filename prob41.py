a8ffcd00b785ac8c930d1c4a61f1ddb1bf37c165
Morgenrode <dsg14@zips.uakron.edu>
2015-01-18 21:00:29 -0600

    finished prob 42

---
+++ prob41.py
@@ -0,0 +1,3 @@
+'''prob41: find the largest n-digit pandigital prime
+reasoning: only 10 digits, 0-9, thus we only need to check primes smaller than 11 digits long'''
+# sieve of Eratosthenes won't work, too much memory needed
---
+++ prob42.py
@@ -0,0 +1,27 @@
+'''prob42: with letters corresponding to their alphabetical position,
+find how many words in a given list are "triangle" words'''
+
+import string
+encoding = dict(zip(string.ascii_uppercase, [ord(c) % 32 \
+    for c in string.ascii_uppercase]))
+
+word_vals = []
+with open('words.txt', 'r') as f:
+    for word in f.read().split(','):
+        word_vals.append(sum(\
+        [encoding[c] for c in word.strip('\"')])) 
+
+triangles = set()
+n = 1
+while n <= max(word_vals):
+    triangles.add(int((n*(n + 1)/2)))
+    n += 1
+
+counter = 0
+for i in word_vals: 
+    if i in triangles:
+        counter += 1
+
+print(counter)
+
+
